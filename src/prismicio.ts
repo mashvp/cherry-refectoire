93172c97a4f20298c323894ac270fc896a602eda
BPecou <pecou.brice@gmail.com>
2024-10-17 12:01:51 +0200

    Test

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -1,7 +1,7 @@
 import * as prismic from "@prismicio/client";
 import * as prismicNext from "@prismicio/next";
 
-import config from "../slicemachine.config.json";
+import config from "../slicemachine.config";
 
 /**
  * The project's Prismic repository name.
